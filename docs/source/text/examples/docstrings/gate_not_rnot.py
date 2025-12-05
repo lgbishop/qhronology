eb39e0fe3ced7a74d74f66d345de747cf0cbc37e
lgbishop <lachlan.bishop@hotmail.com>
2025-12-05 21:21:59 +1000

    docs: change RNOT gate diagram to block family

--- docs/source/text/examples/docstrings/gate_not_rnot.py
+++ docs/source/text/examples/docstrings/gate_not_rnot.py
@@ -1,6 +1,6 @@
 from qhronology.quantum.gates import *
 
-RNOT = Not(targets=[0], exponent=sp.Rational(1, 2))
+RNOT = Not(targets=[0], exponent=sp.Rational(1, 2), label="√NOT", family="GATE")
 RNOT.diagram()
 print(repr(RNOT.output()))
 RNOT.print()
