e9891b15b5c970514002d4a6204829c685adab60
lgbishop <lachlan.bishop@hotmail.com>
2026-07-09 23:42:28 +1000

    package: remove unnecessary conversion to SymPy matrix

--- src/qhronology/quantum/gates.py
+++ src/qhronology/quantum/gates.py
@@ -158,7 +158,7 @@ class QuantumGate(QuantumObject):
         if spec is None:
             spec = sp.eye(dim)
         else:
-            spec_num_systems = count_systems(to_matrix(spec), dim)
+            spec_num_systems = count_systems(spec, dim)
         num_systems = (
             (max(spec_num_systems, max(targets + controls + anticontrols) + 1))
             if num_systems is None
